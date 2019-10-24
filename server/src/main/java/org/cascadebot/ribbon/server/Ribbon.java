1cc4bf392e84ccf8818072c76b7556fdf86db307
William Oldham <wegg7250@gmail.com>
2019-10-24 22:54:07 +0100

    Rename main class to be more descriptive

--- server/src/main/java/org/cascadebot/ribbon/server/Ribbon.java
+++
@@ -1,9 +0,0 @@
-package org.cascadebot.ribbon.server;
-
-public class Ribbon {
-
-    public static void main(String[] args) {
-
-    }
-
-}
---
+++ server/src/main/java/org/cascadebot/ribbon/server/RibbonServer.java
@@ -0,0 +1,14 @@
+package org.cascadebot.ribbon.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RibbonServer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(RibbonServer.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
